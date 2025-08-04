59aa550db6a238bd463db4827f3a2c851c0e49f6
Jana Abozeid <janahisham2003@gmail.com>
2025-08-04 21:17:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,35 @@
-// Edit this to set the staff password
-const staffPassword = "brdhnd052"; // <-- You can change this password
 
-// Get the code from the URL (e.g. /BR123X)
-const urlParts = window.location.pathname.split("/");
-const code = urlParts[urlParts.length - 1].toUpperCase();
-document.getElementById("promoCode").innerText = code;
+    const staffPassword = "brndhnd052";
 
-// Check if code is already used in localStorage
-if (localStorage.getItem("used_" + code)) {
-  document.getElementById("usedMessage").style.display = "block";
-  document.querySelector("input").style.display = "none";
-  document.querySelector("button").style.display = "none";
-}
+    const urlParts = window.location.pathname.split("/");
+    const code = urlParts[urlParts.length - 1].toUpperCase();
+    document.getElementById("promoCode").innerText = code;
 
-function checkPassword() {
-  const input = document.getElementById("password").value;
-  if (input === staffPassword) {
-    document.getElementById("codeContainer").style.display = "block";
-  } else {
-    alert("Неверный пароль");
-  }
-}
+    const usedKey = `brandhand_used_${code}`;
+    const isUsed = localStorage.getItem(usedKey);
 
-function markUsed() {
-  localStorage.setItem("used_" + code, "true");
-  alert("Код отмечен как использованный.");
-  location.reload();
-}
+    if (isUsed === "true") {
+      document.getElementById("usedMessage").style.display = "block";
+      document.getElementById("codeContainer").style.display = "none";
+    }
+
+    function checkPassword() {
+      const input = document.getElementById("password").value;
+      if (input === staffPassword) {
+        if (isUsed === "true") {
+          document.getElementById("usedMessage").style.display = "block";
+        } else {
+          document.getElementById("codeContainer").style.display = "block";
+        }
+      } else {
+        alert("Неверный пароль");
+      }
+    }
+
+    function markUsed() {
+      localStorage.setItem(usedKey, "true");
+      document.getElementById("promoCode").innerText = "Код использован";
+      document.getElementById("usedMessage").style.display = "block";
+      document.getElementById("codeContainer").style.display = "none";
+    }
+  
